9a293c2443a263f02c9cdce80d71e605f95f81c9
PowerHaus Dev <dev@powerhaus.com>
2026-06-19 08:25:00 -0500

    Bump to 2.9.19

---
+++ bump_version_19.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+
+let pkg = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+
+let versionParts = pkg.version.split('.');
+versionParts[2] = parseInt(versionParts[2]) + 1;
+pkg.version = versionParts.join('.');
+
+fs.writeFileSync('package.json', JSON.stringify(pkg, null, 4));
+console.log('Versión actualizada a ' + pkg.version);
